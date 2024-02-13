293b8fa1381ba29849af9f51391ff96bb0a370b0
Jacques9 <104220884+Jacques9@users.noreply.github.com>
2024-02-13 16:59:27 +0200

    first1

--- Backend/server.ts
+++ Backend/server.ts
@@ -1,2 +1,13 @@
 // import Elysia from "elysia"
-// import { PrismaClient } from "@prisma/client"
+import { PrismaClient } from "@prisma/client"
+
+const prisma = new PrismaClient();
+
+const createNull = await prisma.users.create({
+  data: {
+    email: 'user1@prisma.io',
+    name: "Andrei1",
+  },
+})
+
+console.log(createNull);
