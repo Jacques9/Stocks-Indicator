fd848272833d345c158d311450ca4edce7ef18dc
Jacques9 <104220884+Jacques9@users.noreply.github.com>
2024-02-13 18:30:48 +0200

    structure + cors + swagger

---
+++ Backend/src/pluginSetup.ts
@@ -0,0 +1,4 @@
+import { swagger } from "@elysiajs/swagger"
+import { cors } from "@elysiajs/cors"
+
+export { swagger, cors }
--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -1,13 +1,16 @@
 import { Elysia } from "elysia"
 import { PrismaClient } from "@prisma/client"
 
+import { swagger, cors } from "./pluginSetup.ts";
+
 const prisma = new PrismaClient();
 const app = new Elysia();
 
-app.get("/", () => "Hello Elysia");
+app.use(swagger())
+app.use(cors())
 
 app.listen(3000, () => {
-    console.log("Server is listening on port 8080")
+    console.log("Server is listening on port 3000")
 });
 
 export { app, prisma };
