92bbee429af3ad87c84659fd27a16841f1581f4b
Jacques9 <104220884+Jacques9@users.noreply.github.com>
2024-03-05 17:00:42 +0200

    .

--- Backend/api/api.ts
+++ Backend/api/api.ts
@@ -1,20 +1,21 @@
-import { Elysia } from "elysia"
-import { PrismaClient } from "@prisma/client"
-// import { userController } from "../controllers/user.controller.ts"
-
+import { Elysia } from "elysia";
+import { PrismaClient } from "@prisma/client";
+import { userController } from "../controllers/user.controllers";
 import { swagger } from "@elysiajs/swagger";
+import { helmet } from 'elysia-helmet';
 import cors from "@elysiajs/cors";
 
-// const prisma = new PrismaClient();
+const prisma = new PrismaClient();
 const app = new Elysia();
 
+app.use(helmet())
 app.use(swagger())
 app.use(cors())
 
-// app.use(userController as any)
+app.use(userController as any)
 
 app.listen(3000, () => {
     console.log("Server is listening on port 3000")
 });
 
-// export { app, prisma };
+export { app, prisma };
